ba0e18093d627575c87286a302f92c90fe57452a
Msuf123 <88485149+Msuf123@users.noreply.github.com>
2026-03-13 22:38:59 +0530

    Fix duplicate loop declaration in arraySelf.js

--- Practice/arraySelf.js
+++ Practice/arraySelf.js
@@ -1,7 +1,7 @@
 function n(nums){
 let result=new Array(nums.length-1)
 result[0]=1
-for(let i=1;i<nums.length;i++){
+for(let i=1;i<nums.length;i++){ 
   result[i]=nums[i-1]*result[i-1]
 }
 let right=1
